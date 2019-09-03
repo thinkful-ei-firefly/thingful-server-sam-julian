67be808584743d3d7d084fbba3cd37db35d037c2
Sam Walker <jack.nasal@gmail.com>
2019-09-03 15:25:59 -0400

    added authentication

---
+++ src/userAuth.js
@@ -0,0 +1,32 @@
+function requireAuth(req, res, next) {
+  const authToken = req.get('Authorization') || ''
+
+  if (!authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({error: 'Missing basic token'})
+  }
+
+  const basicToken = authToken.slice('basic '.length, authToken.length);
+
+  const [token_username, token_password] = Buffer
+    .from(basicToken, 'base64')
+    .toString()
+    .split(':')
+
+  if (!token_username || !token_password) {
+    return res.status(401).json({error: 'Unauthorized request (no username/password)'})
+  }
+
+  req.app.get('db')('thingful_users')
+    .where({user_name: token_username})
+    .first()
+    .then(user => {
+      if (!user || user.password !== token_password) {
+        return res.status(401).json({error: 'Unauthorized request(no user match or wrong password)'})
+      }
+      next()
+    })
+    .catch(next)
+}
+
+
+module.exports = requireAuth
